f80e8254cea5b735c56dd77df4b181322a45a593
Inplex-sys <69421356+Inplex-sys@users.noreply.github.com>
2022-09-23 17:53:01 +0200

    Add files via upload

---
+++ combos.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "headers/combos.h"
+
+int cindex = 0;
+Combo *combos = NULL;
+
+void combo_add(char *username, char *password)
+{
+    if (combos == NULL)
+        combos = calloc(1, sizeof(Combo));
+    else
+        combos = realloc(combos, (cindex + 2) * sizeof(Combo));
+
+    combos[cindex].username = username;
+    combos[cindex].password = password;
+
+    combos[cindex].username_len = strlen(username);
+    combos[cindex].password_len = strlen(password);
+    cindex++;
+}
+
+void combos_init(void)
+{
+    combo_add("root", "");
+    combo_add("admin", "");
+    combo_add("root", "root");
+    combo_add("admin", "admin");
+    combo_add("user", "user");
+    combo_add("ubnt", "ubnt");
+    combo_add("ubuntu", "ubuntu");
+    combo_add("guest", "guest");
+    combo_add("support", "support");
+    combo_add("default", "default");
+    combo_add("test", "test");
+    combo_add("fake", "fake");
+    combo_add("root", "admin");
+    combo_add("admin", "root");
+    combo_add("root", "123");
+    combo_add("root", "1234");
+    combo_add("root", "12345");
+    combo_add("root", "123456");
+    combo_add("root", "changeme");
+    combo_add("admin", "changeme");
+    combo_add("guest", "1234");
+    combo_add("guest", "12345");
+    combo_add("guest", "123456");
+    combo_add("admin", "1234");
+    combo_add("admin", "12345");
+    combo_add("admin", "123456")
+    combo_add("hikvision", "hikvision");
+    combo_add("root", "toor");
+    combo_add("", "");
+    combo_add("operator", "operator");
+    combo_add("ftp", "ftp");
+    combo_add("root", "888888");
+    combo_add("default", "");
+    combo_add("1234", "1234");
+}
